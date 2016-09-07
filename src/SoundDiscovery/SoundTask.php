da08fdd3570815ceb8adf289a568425b60e5d02c
Muqsit Rayyan <hackhack05@gmail.com>
2016-09-07 20:16:25 +0300

    Update SoundTask.php

--- src/SoundDiscovery/SoundTask.php
+++ src/SoundDiscovery/SoundTask.php
@@ -3,14 +3,13 @@ namespace SoundDiscovery;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
 use pocketmine\math\Vector3;
-use pocketmine\Player;
 class SoundTask extends PluginTask{
 
-	public function __construct(SoundDiscovery $plugin, Player $player){
-	  $this->plugin = $plugin;
-		$this->player = $player;
-		parent::__construct($plugin);
-	}
+  public function __construct(SoundDiscovery $plugin, Player $player){
+    $this->plugin = $plugin;
+    $this->player = $player;
+    parent::__construct($plugin);
+  }
   
   public function onRun($currentTick){
     $this->player->getLevel()->addSound(new StrangeSound(new Vector3($this->player->x, $this->player->y, $this->player->z)));
